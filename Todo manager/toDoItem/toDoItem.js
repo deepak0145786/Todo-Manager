e6e065d4a7d56de26d71ebc5d6f4b7f52585b9e3
Devid <33474493+deepak0145786@users.noreply.github.com>
2020-05-02 17:13:29 +0530

    added pubsub for update and delete.

--- Todo manager/toDoItem/toDoItem.js
+++ Todo manager/toDoItem/toDoItem.js
@@ -1,12 +1,13 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 import updateTodo from '@salesforce/apex/ToDoController.updateTodo';
 import deleteTodo from '@salesforce/apex/ToDoController.deleteTodo';
+import { CurrentPageReference } from 'lightning/navigation';
+import { fireEvent } from 'c/pubsub';
 
 export default class ToDoItem extends LightningElement {
     @api todoitem;
-    connectedCallback(){
-        console.log('ToDoItem.js connectedCallback=>', JSON.stringify(this.todoitem));
-    }
+    
+    @wire(CurrentPageReference) pageRef;
     get containerClass(){
         return this.todoitem.done ? "todo completed" : "todo upcoming";
     }
@@ -27,17 +28,18 @@ export default class ToDoItem extends LightningElement {
                 console.log('updated successfully!') ;
                 const updateEvent = new CustomEvent('update');
                 this.dispatchEvent(updateEvent);
+                fireEvent(this.pageRef, 'refreshTodoList');
             }).catch(error=>{
                 console.error('Error in updatehandler=>', error);
             });
-
-            
     }
     deleteHandler(){
         deleteTodo({todoId: this.todoitem.todoId}).then(response=>{
             console.log('Deleted successfully!');
             const deleteEvent = new CustomEvent('deletetodo');
                 this.dispatchEvent(deleteEvent);
+                
+                fireEvent(this.pageRef, 'refreshTodoList');
                 console.log('Deleted successfully!');
         }).catch(error=>{
             console.error('Error in deleteHandler=>', error);
